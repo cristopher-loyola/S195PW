b3eb974607999b79f683c467dcf34122307e8b1f
cristopher-loyola <157190820+cristopher-loyola@users.noreply.github.com>
2024-10-16 22:39:16 -0600

    Creacion de una vista de inicio con mis datos

--- PruebaRepasos/resources/views/welcome.blade.php
+++ PruebaRepasos/resources/views/welcome.blade.php
@@ -8,7 +8,12 @@
 
 </head>
 <body>
-<button type="button" class="btn btn-info">Info</button>
-<button type="button" class="btn btn-dark">Dark</button>
+<div style="text-align: center; margin-top: 40vh;">
+    <h1>Cristopher Antonio Loyola Martinez</h1>
+    <h2>Programacion Web</h2>
+    <h3>Acividad de repaso 1</h3>
+    <br>
+    <a href="/Repaso1" class="btn btn-primary btn-start">Repaso 1</a>
+</div>
 </body>
 </html>
